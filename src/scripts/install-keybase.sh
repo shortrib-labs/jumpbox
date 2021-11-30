5c0aa3dd73bffcddae4972509738b81efd74db99
Chuck D'Antonio <cdantonio@pivotal.io>
2021-11-30 18:44:19 +0000

    Installs keybase CLI binaries

---
+++ src/scripts/install-keybase.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S bash -e
+
+if [[ ! -f ${WORK_DIR}/keybase_amd64.deb ]] ; then
+  (cd ${WORK_DIR} && curl --silent --remote-name https://prerelease.keybase.io/keybase_amd64.deb)
+fi
+ar -p ${WORK_DIR}/keybase_amd64.deb data.tar.xz | 
+  xz --decompress | 
+  sudo tar -C / --strip-components 1 -xf - ./usr/bin/keybase ./usr/lib/systemd/user/keybase.service 
