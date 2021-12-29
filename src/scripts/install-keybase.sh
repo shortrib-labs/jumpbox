15a60aa78c097dd8a3c398985ef2824eedea5994
Chuck D'Antonio <cdantonio@pivotal.io>
2021-12-29 01:32:09 +0000

    Updates keybase script to use explicit work directory

--- src/scripts/install-keybase.sh
+++ src/scripts/install-keybase.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env -S bash -e
 
+WORK_DIR=/tmp
 if [[ ! -f ${WORK_DIR}/keybase_amd64.deb ]] ; then
   (cd ${WORK_DIR} && curl --silent --remote-name https://prerelease.keybase.io/keybase_amd64.deb)
 fi
 ar -p ${WORK_DIR}/keybase_amd64.deb data.tar.xz | 
   xz --decompress | 
   sudo tar -C / --strip-components 1 -xf - ./usr/bin/keybase ./usr/lib/systemd/user/keybase.service 
+rm ${WORK_DIR}/keybase_amd64.deb
