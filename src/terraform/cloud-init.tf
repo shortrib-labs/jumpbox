edfaf1b46cba6816389483669df54f01aa08b0f8
Chuck D'Antonio <chuck@crdant.io>
2022-02-01 20:56:35 +0000

    Assures the instance cloud init gets the user SSH keys

--- src/terraform/cloud-init.tf
+++ src/terraform/cloud-init.tf
@@ -7,6 +7,7 @@ data "carvel_ytt" "user_data" {
     "ssh.authorized_key" = var.ssh_authorized_keys.0
     "hashed_password" = var.hashed_password
     "mac_address" = var.mac_address
+    "ssh.user_authorized_keys" = var.user_authorized_keys
   }
 
   ignore_unknown_comments = true
